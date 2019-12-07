38c8269747592f149f3e0fa9872668883c2c9b16
thiernodiallo222 <thiernodiallo864@gmail.com>
2019-12-06 21:42:08 -0500

    completed some of the project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 // Your code goes here
-// 
-// Thierno Diallo
+
+
+
+let home = document.querySelector('header div nav .nav-link');
+home.addEventListener("mouseover", function () {
+    alert('Hey there, this link takes you to the home page !');
+});
+
+let topImage = document.querySelector('div header img');
+
+topImage.addEventListener("mouseenter", function () {
+    // topImage.style.width = '1000000px';
+    topImage.style.height = '400px';
+});
+
+topImage.addEventListener("mouseleave", function () {
+    topImage.style.width = '100%';
+    topImage.style.height = 'auto';
+});
+
+let button = document.querySelector('.btn');
+
+button.addEventListener("click", function(){
+    button.style.color = 'white';
+    button.style.backgroundColor = 'black';
+    button.innerHTML ="All Set !"
+});
+
+button.addEventListener("mouseleave", function () {
+    button.style.color = 'white';
+    button.style.background ='#17A2B8';
+    button.innerHTML ="Sign Me Up !"
+})
+
